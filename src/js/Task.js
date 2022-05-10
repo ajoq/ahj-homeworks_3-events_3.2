a03f29f8950810432ef662f7db9e52196d370bb2
Konstantin Beloglazov <ajoq@ya.ru>
2022-05-10 16:56:10 +0300

    dev 1.1.6 lint

--- src/js/Task.js
+++ src/js/Task.js
@@ -1,11 +1,11 @@
 export default class Task {
-    constructor(text) {
-        this.text = text;
-        this.id = 0;
-        this.pinned = false;
-    }
+  constructor(text) {
+    this.text = text;
+    this.id = 0;
+    this.pinned = false;
+  }
 
-    setId(number) {
-        this.id = number;
-    }
+  setId(number) {
+    this.id = number;
+  }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-new */
 import TasksList from './TasksList';
 
-const taskList = new TasksList();
+new TasksList();
